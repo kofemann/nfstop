54bb8558dda52e61a228790f722ec8969d460855
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2017-04-11 10:55:03 +0200

    add an option to list available interfaces

--- main.go
+++ main.go
@@ -27,6 +27,7 @@ func (w *NopWorker) OnPacket(data []byte, ci *gopacket.CaptureInfo) {
 
 var iface = flag.String("i", ANY_DEVICE, "name of `interface` to listen")
 var filter = flag.String("f", NFS_FILTER, "capture `filter` in libpcap filter syntax")
+var listInterfaces = flag.Bool("D", false, "print list of interfaces and exit")
 
 func main() {
 
@@ -43,6 +44,19 @@ func main() {
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Failed to initialize sniffer: %v\n", err)
 		os.Exit(1)
+	}
+
+	if *listInterfaces {
+		ifaces, err := pcap.FindAllDevs()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Failed to get list of interfaces: %v\n", err)
+			os.Exit(1)
+		}
+
+		for i, dev := range ifaces {
+			fmt.Printf("%d. %s\n", i+1, dev.Name)
+		}
+		os.Exit(0)
 	}
 
 	isDone := false
